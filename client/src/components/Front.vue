bad459a229403a485c9bbfca71d0eaa6cc0d7e52
Joona Antila <antjoo36@gmail.com>
2019-12-21 01:38:33 +0200

    Initiated Vue-project and added scss-support

---
+++ client/src/components/Front.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Click to copy</h1>
+    <div id="search">
+      <input type="text" name="search" />
+    </div>
+    <div id="emojis"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Front",
+  data() {
+    return {
+      msg: "dd"
+    };
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+</style>
